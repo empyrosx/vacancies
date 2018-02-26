dcd9725ac8e66c1946c88a41489d1a52f520e60a
empyrosx <empyrosx@gmail.com>
2018-02-26 22:23:21 +0300

    Init commit (project skeleton)

---
+++ src/main/java/com/github/empyrosx/vacancies/VacanciesApplication.java
@@ -0,0 +1,12 @@
+package com.github.empyrosx.vacancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanciesApplication.class, args);
+	}
+}
